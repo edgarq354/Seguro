a4a02624d3480c1ad020ae50371d4bd1c9c56fdf
Edgar Elio <edgarq354@gmail.com>
2021-05-03 18:06:40 -0400

    servicios implementados

--- app/src/main/java/com/elisoft/seguro/Bienvenido.java
+++ app/src/main/java/com/elisoft/seguro/Bienvenido.java
@@ -1,5 +1,6 @@
 package com.elisoft.seguro;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,9 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class Bienvenido extends AppCompatActivity {
-
+Button bt_siguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +21,14 @@ public class Bienvenido extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bt_siguiente=findViewById(R.id.bt_siguiente);
+
+        bt_siguiente.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(Bienvenido.this,Permisos.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/elisoft/seguro/Login.java
+++ app/src/main/java/com/elisoft/seguro/Login.java
@@ -1,6 +1,5 @@
 package com.elisoft.seguro;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,26 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.Button;
 
-public class Permisos extends AppCompatActivity {
-    Button bt_siguiente;
+public class Login extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_permisos);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        bt_siguiente=findViewById(R.id.bt_siguiente);
-
-        bt_siguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Permisos.this,Permisos.class));
-            }
-        });
 
     }
 }
